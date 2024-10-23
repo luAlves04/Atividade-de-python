87d505bdd921dd8829464a168271aeccfc667267
luAlves04 <160678946+luAlves04@users.noreply.github.com>
2024-10-23 09:41:47 -0300

    Add files via upload

---
+++ atv3.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def botaoclick():
+    messagebox.showinfo("Parabéns!!", "Botão clicado")
+
+#Configurações da janela principal 
+janela = tk.Tk()
+janela.title("Interface Gráfica com Imagem")
+janela.geometry("800x500")
+
+#Imagem de fundo
+imagem = tk.PhotoImage(file="ohmyga.gif")
+rotulo = tk.Label(janela, image=imagem)
+rotulo.pack()
+
+#Rotulo com uma mensagem abaixo da imagem
+rotuloMensagem = tk.Label(janela, text="Clique no botão")
+rotuloMensagem.pack(pady=10)
+
+#Configuração do botão
+imagemBotão = tk.PhotoImage(file="click.png")
+botao = tk.Button(janela, image=imagemBotão, command=botaoclick).pack(pady=10)
+
+
+janela.mainloop()
